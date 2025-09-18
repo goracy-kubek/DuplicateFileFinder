046542930647f5ae9ab63815c649923b89ddeb0d
Fs <123@123.123>
2025-09-19 04:02:15 +0500

    GUI

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
 
 func main() {
-	fmt.Println("Hello")
+	a := app.New()
+	w := a.NewWindow("Duplicate finder")
+	w.Resize(fyne.NewSize(600, 400))
+
+	w.SetContent(container.NewVBox(
+		searchField(),
+		widget.NewSeparator(),
+	))
+	w.ShowAndRun()
+}
+
+func searchField() *fyne.Container {
+	search := widget.NewEntry()
+	search.SetPlaceHolder("Start folder path...")
+
+	button := widget.NewButton("Search", func() {
+		println("Searching")
+	})
+
+	return container.NewBorder(nil, nil, nil, button, search)
 }
